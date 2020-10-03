f5c90c73e79b9fd6f8c94d3e73d59e055d1c2493
damaruwa <72242328+damaruwa@users.noreply.github.com>
2020-10-03 10:49:02 +0530

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -7,3 +7,25 @@ function myFunction() {
     }
   }
 
+/* video player script*/
+
+var myVideo = document.getElementById("video1"); 
+
+function playPause() { 
+  if (myVideo.paused) 
+    myVideo.play(); 
+  else 
+    myVideo.pause(); 
+} 
+
+function makeBig() { 
+    myVideo.width = 560; 
+} 
+
+function makeSmall() { 
+    myVideo.width = 270; 
+} 
+
+function makeNormal() { 
+    myVideo.width = 320; 
+} 
